66d5e8f00df6bdc6f631c5517691b38ce6010c79
Bala Bharathy.A.P <163611197+balabharathy127@users.noreply.github.com>
2026-07-06 14:15:47 +0530

    Delete database.py

--- database.py
+++
@@ -1,7 +0,0 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-
-db_url = "postgresql://postgres:admin@localhost:5432/postgres"
-
-engine = create_engine(db_url)
-session = sessionmaker(bind=engine, autocommit=False, autoflush=False)
